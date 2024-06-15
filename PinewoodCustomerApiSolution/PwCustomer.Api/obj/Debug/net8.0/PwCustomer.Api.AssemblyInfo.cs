bc2f6088d02edd4e381e7d4c51b5454ded6b6d0e
SenthilRajaK <54350680+ksenthilraajaa@users.noreply.github.com>
2024-06-15 10:31:15 +0100

    Checkin
    checkin

--- PinewoodCustomerApiSolution/PwCustomer.Api/obj/Debug/net8.0/PwCustomer.Api.AssemblyInfo.cs
+++ PinewoodCustomerApiSolution/PwCustomer.Api/obj/Debug/net8.0/PwCustomer.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PwCustomer.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc51b3b3666a250cd3d26122b14987eea8e4ee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PwCustomer.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PwCustomer.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PinewoodCustomerApiSolution/PwCustomer.Domain/obj/Debug/net8.0/PwCustomer.Domain.AssemblyInfo.cs
+++ PinewoodCustomerApiSolution/PwCustomer.Domain/obj/Debug/net8.0/PwCustomer.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PwCustomer.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc51b3b3666a250cd3d26122b14987eea8e4ee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PwCustomer.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PwCustomer.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
